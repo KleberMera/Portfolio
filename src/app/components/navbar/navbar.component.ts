ebbc2946003c12690d0d1e026973c413729cc388
KleberMera <klebermera2016@gmail.com>
2024-09-01 16:51:43 -0500

    styles id scroll

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,14 +13,12 @@ export class NavbarComponent {
   @ViewChild('sidebar') sidebar!: ElementRef;
 
   navLinks = [
-    { id: 'home', name: 'Home' },
     { id: 'about', name: 'Sobre mí' },
     { id: 'skills', name: 'Habilidades' },
     { id: 'projects', name: 'Proyectos' },
     { id: 'education', name: 'Educación' },
-    { id: 'contact', name: 'Contacto' }
+    { id: 'contact', name: 'Contacto' },
   ];
-
 
   scrollToSection(sectionId: string): void {
     const section = document.getElementById(sectionId);
